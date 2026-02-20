eed6b80741bb39832e180a56b1a0814b65753a7d
João Victor Souza <souza.joao1997@gmail.com>
2026-02-20 17:09:13 -0300

    refactor: enhance disconnection handling in BaileysStartupService
    - Added DisconnectReason.connectionReplaced to the list of codes that prevent reconnection.
    - Improved clarity and maintainability of the disconnection logic in the BaileysStartupService.

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -531,7 +531,13 @@ export class BaileysStartupService extends ChannelStartupService {
       }
 
       const statusCode = (lastDisconnect?.error as Boom)?.output?.statusCode;
-      const codesToNotReconnect = [DisconnectReason.loggedOut, DisconnectReason.forbidden, 402, 406];
+      const codesToNotReconnect = [
+        DisconnectReason.loggedOut,
+        DisconnectReason.forbidden,
+        DisconnectReason.connectionReplaced,
+        402,
+        406,
+      ];
       const shouldReconnect = !codesToNotReconnect.includes(statusCode);
 
       this.logger.info({
